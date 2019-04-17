cac277c96cc83c255d2d7470724873b92583a600
Daniel Gozalo <dgozalo@cloudbees.com>
2019-04-17 14:03:14 +0200

    feature: added GetApps and UpgradeApps tests

--- apps.go
+++ apps.go
@@ -39,6 +39,8 @@ func AppTest(testAppName string, version string) bool {
 		})
 
 		_ = T.AddAppTests(testAppName, version)
+		_ = T.GetAppsTests(testAppName)
+		_ = T.UpgradeAppTests(testAppName)
 		_ = T.DeleteAppTests(testAppName)
 
 	})
@@ -48,11 +50,10 @@ func AppTest(testAppName string, version string) bool {
 func (t *Test) AddAppTests(testAppName string, version string) bool {
 	return Describe("Given valid parameters", func() {
 		Context("when running jx add app "+testAppName, func() {
-
 			It("Ensure the app is added\n", func() {
 				By("The App resource does not exist before creation\n")
 				c := "jx"
-				args := []string{"get", "app", testAppName}
+				args := []string{"get", "app", testAppName, "--namespace", "jx"}
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 1, c, args...)
 				By("Add app exits with signal 0\n")
 				c = "jx"
@@ -63,7 +64,50 @@ func (t *Test) AddAppTests(testAppName string, version string) bool {
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
 				By("The App resource exists after creation\n")
 				c = "jx"
-				args = []string{"get", "app", testAppName}
+				args = []string{"get", "app", testAppName, "--namespace", "jx"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+			})
+		})
+	})
+}
+
+func (t *Test) GetAppsTests(testAppName string) bool {
+	return Describe("Given valid parameters", func() {
+		Context("when running jx get apps "+testAppName, func() {
+			It("Ensure it returns correct data\n", func() {
+				By("There is at least one app created\n")
+				c := "jx"
+				args := []string{"get", "app", testAppName, "--namespace", "jx"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Can export the data as yaml\n")
+				args = []string{"get", "app", testAppName, "--namespace", "jx", "-o", "yaml"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Can export the data as json\n")
+				args = []string{"get", "app", testAppName, "--namespace", "jx", "-o", "json"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Can get the detailed status of the app\n")
+				args = []string{"get", "app", testAppName, "--namespace", "jx", "--status"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Can export the detailed status data as yaml\n")
+				args = []string{"get", "app", testAppName, "--namespace", "jx", "--status", "-o", "yaml"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Can export the detailed status data as json\n")
+				args = []string{"get", "app", testAppName, "--namespace", "jx", "--status", "-o", "json"}
+			})
+		})
+	})
+}
+
+func (t *Test) UpgradeAppTests(testAppName string) bool {
+	return Describe("Given valid parameters", func() {
+		Context("when running jx upgrade app "+testAppName, func() {
+			It("Ensure it is upgraded\n", func() {
+				By("The App resource exists before upgrade\n")
+				c := "jx"
+				args := []string{"get", "app", testAppName, "--namespace", "jx"}
+				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
+				By("Upgrade an app exists with signal 0\n")
+				args = []string{"upgrade", "app", testAppName, "--namespace", "jx"}
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
 			})
 		})
@@ -77,15 +121,15 @@ func (t *Test) DeleteAppTests(testAppName string) bool {
 			It("Ensure it is deleted\n", func() {
 				By("The App resource exists before deletion\n")
 				c := "jx"
-				args := []string{"get", "app", testAppName}
+				args := []string{"get", "app", testAppName, "--namespace", "jx"}
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
 				By("Delete app exits with signal 0\n")
 				c = "jx"
-				args = []string{"delete", "app", testAppName}
+				args = []string{"delete", "app", testAppName, "--namespace", "jx"}
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 0, c, args...)
 				By("The App resource was removed\n")
 				c = "jx"
-				args = []string{"get", "app", testAppName}
+				args = []string{"get", "app", testAppName, "--namespace", "jx"}
 				t.ExpectCommandExecution(t.WorkDir, TimeoutAppTests, 1, c, args...)
 			})
 		})

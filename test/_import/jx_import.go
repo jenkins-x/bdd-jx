952843043842c579a973ab3e345f505037dbffed
Jose Armesto <github@armesto.net>
2019-07-09 12:46:52 +0200

    feat: adding scenario for importing go application

--- test/_import/jx_import.go
+++ test/_import/jx_import.go
@@ -85,6 +85,56 @@ var _ = Describe("Import Application", func() {
 					})
 				}
 			})
+			It("creates a Go application from the specified folder and promotes it to staging", func() {
+				destDir := T.WorkDir + "/" + T.ApplicationName
+				url := "https://github.com/jenkins-x-quickstarts/golang-http.git"
+
+				By(fmt.Sprintf("calling git clone %s", url), func() {
+					_, err := git.PlainClone(destDir, false, &git.CloneOptions{
+						URL:      url,
+						Progress: GinkgoWriter,
+					})
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				By("removing the .git directory", func() {
+					err := os.RemoveAll(destDir + "/.git")
+					utils.ExpectNoError(err)
+					Expect(destDir + "/.git").ToNot(BeADirectory())
+				})
+
+				By("updating the pom.xml to have the correct application name", func() {
+					err := utils.ReplaceElement(filepath.Join(destDir, "Makefile"), "REPLACE_ME_APP_NAME", T.ApplicationName, 1)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				gitProviderUrl, err := T.GitProviderURL()
+				Expect(err).NotTo(HaveOccurred())
+				args := []string{"import", destDir, "-b", "--org", T.GetGitOrganisation(), "--git-provider-url", gitProviderUrl}
+				argsStr := strings.Join(args, " ")
+				By(fmt.Sprintf("running jx %s", argsStr), func() {
+					T.ExpectJxExecution(T.WorkDir, helpers.TimeoutSessionWait, 0, args...)
+				})
+
+				T.TheApplicationShouldBeBuiltAndPromotedViaCICD(200)
+
+				if T.DeleteApplications() {
+					args = []string{"delete", "application", "-b", T.ApplicationName}
+					argsStr := strings.Join(args, " ")
+					By(fmt.Sprintf("deleting the application by calling jx %s", argsStr), func() {
+						T.ExpectJxExecution(T.WorkDir, helpers.TimeoutSessionWait, 0, args...)
+					})
+
+				}
+
+				if T.DeleteRepos() {
+					args = []string{"delete", "repo", "-b", "-g", gitProviderUrl, "-o", T.GetGitOrganisation(), "-n", T.ApplicationName}
+					argsStr := strings.Join(args, " ")
+					By(fmt.Sprintf("deleting the repo by calling jx %s", argsStr), func() {
+						T.ExpectJxExecution(T.WorkDir, helpers.TimeoutSessionWait, 0, args...)
+					})
+				}
+			})
 		})
 	})
 })

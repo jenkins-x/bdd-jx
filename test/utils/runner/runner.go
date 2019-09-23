d36a9cb8f550aac43b22a4d0eacc6cacdd39041a
Hardy Ferentschik <hardy@ferentschik.de>
2019-09-23 21:45:14 +0200

    feat: adding BDD_JX to be able to specify jx binary to use

--- test/utils/runner/runner.go
+++ test/utils/runner/runner.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	jx = "jx"
+	Jx = "jx"
 )
 
 var (
@@ -57,7 +57,8 @@ func (r *JxRunner) run(out io.Writer, errOut io.Writer, args ...string) error {
 	if testing.Verbose() {
 		utils.LogInfof("\033[1mRUNNER:\033[0mAbout to execute jx %s in %s with timeout %v expecting exit code %d\n", strings.Join(args, " "), r.cwd, r.timeout, r.exitCode)
 	}
-	command := exec.Command(jx, args...)
+
+	command := exec.Command(r.jxBin(), args...)
 	command.Dir = r.cwd
 	session, err := gexec.Start(command, out, errOut)
 	if err != nil {
@@ -69,9 +70,17 @@ func (r *JxRunner) run(out io.Writer, errOut io.Writer, args ...string) error {
 		utils.LogInfof("\033[1mRUNNER:\033[0mExecution completed with exit code %d\n", session.ExitCode())
 	}
 	if session.ExitCode() != r.exitCode {
-		return errors.Errorf("expected exit code %d but got %d whilst running command %s %s", r.exitCode, session.ExitCode(), jx, strings.Join(args, " "))
+		return errors.Errorf("expected exit code %d but got %d whilst running command %s %s", r.exitCode, session.ExitCode(), Jx, strings.Join(args, " "))
 	}
 	return nil
+}
+
+func (r *JxRunner) jxBin() string {
+	jxBin, set := os.LookupEnv("BDD_JX")
+	if !set {
+		jxBin = Jx
+	}
+	return jxBin
 }
 
 // Run runs a jx command
@@ -105,7 +114,7 @@ func (r *JxRunner) RunWithOutput(args ...string) (string, error) {
 func RemoveCoverageText(s string, args ...string) string {
 	coverageOutput := coverageOutputRegex.FindStringSubmatch(s)
 	if len(coverageOutput) == 3 {
-		utils.LogInfof("when running %s %s coverage was %s\n", jx, strings.Join(args, " "), coverageOutput[2])
+		utils.LogInfof("when running %s %s coverage was %s\n", Jx, strings.Join(args, " "), coverageOutput[2])
 	}
 	answer := coverageOutputRegex.ReplaceAllString(s, "")
 	return strings.TrimSpace(answer)

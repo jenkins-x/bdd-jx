68f2dd024574e0b128741f99a373607837b08ef0
Romain Verduci <rverduci@cloudbees.com>
2019-12-13 16:17:02 +0100

    chore: fix JX_APP_UI_VERSION env variable name

--- test/suite/jxui/helpers.go
+++ test/suite/jxui/helpers.go
@@ -5,10 +5,8 @@ import (
 )
 
 var (
-	includeApps = "jx-app-jacoco:0.0.100"
-
 	uiAppName    = "jx-app-ui"
-	uiAppVersion = utils.GetEnv("JX_APP_VERSION", "0.0.59")
+	uiAppVersion = utils.GetEnv("JX_APP_UI_VERSION", "0.0.59")
 
 	// Timeout for waiting for jx add app to complete
 	timeoutAppTests = utils.GetTimeoutFromEnv("BDD_TIMEOUT_APP_TESTS", 60)

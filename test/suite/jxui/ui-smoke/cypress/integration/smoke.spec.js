49bf326880bb7f05da41426f5fa472eaeacce2ed
Romain Verduci <rverduci@cloudbees.com>
2020-02-19 11:26:38 +0100

    test: update search test to match the new behaviour

--- test/suite/jxui/ui-smoke/cypress/integration/smoke.spec.js
+++ test/suite/jxui/ui-smoke/cypress/integration/smoke.spec.js
@@ -54,8 +54,8 @@ describe('Smoke Tests', function() {
             cy.get('[data-test=buildlist-search]').within(() => {
                 cy.get('input').type(testProjectName);
             });
-            cy.get('[data-test=buildlist-build-details]').should('have.length', 1);
-            cy.get('[data-test=buildlist-build-details]').each(element => {
+            cy.get('[data-test=buildlist-build]').should('have.length', 4);
+            cy.get('[data-test=buildlist-build]').each(element => {
                 expect(element.text()).toContain(testProjectName);
             });
         });

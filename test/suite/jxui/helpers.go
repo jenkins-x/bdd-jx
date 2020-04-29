c4ffbb08a3c0c9fd5608a8c5c123d18063572080
Corinne Krych <corinnekrych@gmail.com>
2020-04-29 15:11:53 +0200

    fix: upgrade to lates ui version

--- test/suite/jxui/helpers.go
+++ test/suite/jxui/helpers.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	uiAppName    = "jx-app-ui"
-	uiAppVersion = utils.GetEnv("JX_APP_UI_VERSION", "0.0.59")
+	uiAppVersion = utils.GetEnv("JX_APP_UI_VERSION", "0.1.174")
 
 	// Timeout for waiting for jx add app to complete
 	timeoutAppTests = utils.GetTimeoutFromEnv("BDD_TIMEOUT_APP_TESTS", 60)

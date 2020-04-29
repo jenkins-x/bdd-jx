3560635ae46e686769d571ce6d56327d5747a128
Corinne Krych <corinnekrych@gmail.com>
2020-04-29 16:24:39 +0200

    fix: keep old ui version per default, simplify test to check ui app is installed

--- test/suite/apps/helpers.go
+++ test/suite/apps/helpers.go
@@ -8,7 +8,7 @@ var (
 	includeApps = "jx-app-jacoco:0.0.100"
 
 	uiAppName    = "jx-app-ui"
-	uiAppVersion = utils.GetEnv("JX_APP_VERSION", "0.1.174")
+	uiAppVersion = utils.GetEnv("JX_APP_VERSION", "0.0.59")
 
 	// Timeout for waiting for jx add app to complete
 	timeoutAppTests = utils.GetTimeoutFromEnv("BDD_TIMEOUT_APP_TESTS", 60)
